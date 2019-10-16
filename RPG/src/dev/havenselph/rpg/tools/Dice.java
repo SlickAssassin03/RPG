9aa3bd3c0db6329463d55144e63716d232df2a19
Haven Selph <havenselph@gamail.com>
2019-10-16 17:38:15 -0500

    First Commit

---
+++ RPG/src/dev/havenselph/rpg/tools/Dice.java
@@ -0,0 +1,26 @@
+/**
+ * Class made to easily get small random numbers.
+ */
+
+package dev.havenselph.rpg.tools;
+import java.util.Random;
+
+public class Dice {
+	public static int rollSix() {
+		return roll(5);
+	}
+	
+	public static int rollNine() {
+		return roll(8);
+	}
+	
+	public static int rollTwelve() {
+		return roll(11);
+	}
+	
+	private static int roll(int max) {
+		Random r = new Random();
+		return (r.nextInt(max)+1);
+	}
+
+}
---
+++ RPG/src/dev/havenselph/rpg/tools/package-info.java
@@ -0,0 +1,1 @@
+package dev.havenselph.rpg.tools;
